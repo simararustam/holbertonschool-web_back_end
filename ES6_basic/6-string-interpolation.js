3d8ec22c27cdc7139726a466aa50489c7af58750
simararustam <simararustam@gmail.com>
2024-07-21 12:32:16 +0400

    tamplate literals

--- ES6_basic/6-string-interpolation.js
+++ ES6_basic/6-string-interpolation.js
@@ -8,7 +8,5 @@ export default function getSanFranciscoDescription() {
 
     var {income, gdp, capita} = budget;
   
-    return 'As of ' + year + ', it was the seventh-highest income county in the United States'
-          / ', with a per capita personal income of ' + income + '. As of 2015, San Francisco'
-          / ' proper had a GDP of ' + gdp + ', and a GDP per capita of ' + capita + '.';
+    return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${income}. As of 2015, San Francisco proper had a GDP of ${gdp}, and a GDP per capita of ${capita}.`;
   }
