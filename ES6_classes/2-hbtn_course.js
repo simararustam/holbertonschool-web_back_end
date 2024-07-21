07868b315cf2a4da966dacd8d37e1b9ced382152
simararustam <simararustam@gmail.com>
2024-07-21 17:33:09 +0400

    lint err

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,12 +1,12 @@
 class HolbertonCourse {
     constructor(name, length, students) {
-      this._name = '';
-      this._length = 0;
-      this._students = [];
-  
-      this.name = name;
-      this.length = length;
-      this.students = students;
+        this._name = '';
+        this._length = 0;
+        this._students = [];
+
+        this.name = name;
+        this.length = length;
+        this.students = students;
     }
   
     get name() {
