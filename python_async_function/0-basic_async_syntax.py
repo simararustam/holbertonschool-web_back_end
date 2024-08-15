9f41b15d926fcded8c876e962e710fae9bcfffbe
simararustam <simararustam@gmail.com>
2024-08-15 17:57:25 +0400

    The basics of async updating

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,9 +4,9 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: float = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """Basics of asyn"""
 
     delay = random.uniform(0, max_delay)
-    await asyncio.sleep(random.uniform(0, max_delay))
+    await asyncio.sleep(delay)
     return delay
