cc9a2728cd5fd56989a4811ea4df526b7f2ad00f
simararustam <simararustam@gmail.com>
2024-08-15 16:47:30 +0400

    task 9

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Let's duck type an iterable object"""
+from typing import Iterable, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> Tuple[Sequence, int]:
+    '''
+    Annotate the below function’s parameters \
+        and return values with the appropriate types\
+    '''
+    return [(i, len(i)) for i in lst]
