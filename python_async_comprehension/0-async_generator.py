e83446e2559a348924e0527f4b66500a5f26d1a3
simararustam <simararustam@gmail.com>
2024-08-22 13:27:13 +0400

    documented

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 """Async Generator"""
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """
     The coroutine will loop 10 times, \
     each time asynchronously wait 1 \
