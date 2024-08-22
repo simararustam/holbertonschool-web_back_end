6511445a3f943338a6a64029d8ee0f3bcffe0fd6
simararustam <simararustam@gmail.com>
2024-08-22 13:44:02 +0400

    Async Comprehensions

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,9 +2,9 @@
 
 async_generator = __import__('0-async_generator').async_generator
 """Async Comprehensions"""
+from typing import List
 
-
-async def async_comprehension():
+async def async_comprehension() -> List[int]:
     """Async Comprehensions"""
     result = [i async for i in async_generator()]
     return result
