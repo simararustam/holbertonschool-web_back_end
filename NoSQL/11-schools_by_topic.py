ad07426ae394bfccadacbe4ffcce4a026db16adf
simararustam <simararustam@gmail.com>
2024-08-23 12:24:04 +0400

    task 11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -11,8 +11,6 @@ def schools_by_topic(mongo_collection, topic):
     mongo_collection will be the pymongo collection object
     topic (string) will be topic searched
     '''
-    return [
-        collection for collection in mongo_collection.find(
-            {"topics": topic}
-        )
-    ]
+    return [collection for collection in mongo_collection.find(
+        {"topics": topic}
+        )]
