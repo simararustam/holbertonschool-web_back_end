b01010ea4154243328549335ab1b721e7aaf6d7d
simararustam <simararustam@gmail.com>
2024-08-23 11:48:45 +0400

    list all docs in python

---
+++ NoSQL/8-all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''List all documents in Python'''
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a MongoDB collection.
+
+    Parameters:
+    mongo_collection (pymongo.collection.Collection):
+    The pymongo collection object.
+    Returns:
+    list: A list of all documents in the collection,
+    or an empty list if no documents are found.
+    """
+    docs = list(mongo_collection)
+    return docs
