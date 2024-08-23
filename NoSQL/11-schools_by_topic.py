ac2fc01e63bcfce89eb872593de49e9d470c8245
simararustam <simararustam@gmail.com>
2024-08-23 12:23:29 +0400

    task 11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+Write a Python function that returns the list
+of school having a specific topic:
+'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''
+    Prototype: def schools_by_topic(mongo_collection, topic):
+    mongo_collection will be the pymongo collection object
+    topic (string) will be topic searched
+    '''
+    return [
+        collection for collection in mongo_collection.find(
+            {"topics": topic}
+        )
+    ]
